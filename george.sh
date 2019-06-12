a13864a556c3f91a1ba87bb290835f7a89ad8ef6
kirillkuzin <offkirillkuzin@gmail.com>
2019-06-12 03:55:26 +0300

    Added start messages and norminette command

--- george.sh
+++ george.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 echo "\033[0;36m \n Hi, I'm not Moulinette. I am George. \n \033[0m"
+echo "\033[0;31m \n I am not acquainted with Moulinette personally, so that you communicate with me at your own peril and risk. Communication with me is just an additional guarantee. \n \033[0m"
+echo "\033[0;31m \n Also, you should not get involved in communication with me, repeat independently commands \033[1;31m-norminette -R CheckForbiddenSourceHeader \033[0;31mand \033[1;31mgcc -Wall -Wextra -Werror \n \033[0m"
 git clone $1 user
 cd user
 output="_output"
 for ex in $(find * -type d -name 'ex*')
 do
     echo "\033[0;34m \n\n Checking $ex... \033[0m"
+    echo "\nnorminette -R CheckForbiddenSourceHeader $ex/*"
+    norminette -R CheckForbiddenSourceHeader $ex/*
     if [ -f "../days/$2/$ex/compile.sh" ]; then
         sh ../days/$2/$ex/compile.sh $ex
     else
