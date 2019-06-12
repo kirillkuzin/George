698d162a69687bcf5d4c167f331990c0888190a9
kirillkuzin <offkirillkuzin@gmail.com>
2019-06-12 04:21:12 +0300

    Added file name

--- days/day06/ex02/compile.sh
+++ days/day06/ex02/compile.sh
@@ -1,5 +1,5 @@
-echo "\ngcc -Wall -Wextra -Werror $ex/*.c"
-gcc -Wall -Wextra -Werror $1/*.c
+echo "\ngcc -Wall -Wextra -Werror $ex/ft_print_params.c"
+gcc -Wall -Wextra -Werror $1/ft_print_params.c
 echo "./a.out test1 test2 test3 > user_output"
 ./a.out test1 test2 test3 > user_output
 echo >> user_output
