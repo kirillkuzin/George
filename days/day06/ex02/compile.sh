ad5c8658fb57b63a55acfb6080d0bab0f3c09dc9
Greenbeard Geordi <ggeordi@ox-h2.21-school.ru>
2019-06-12 17:03:09 +0300

    Fixed test for day06/02

--- days/day06/ex02/compile.sh
+++ days/day06/ex02/compile.sh
@@ -1,7 +1,10 @@
-echo "\ngcc -Wall -Wextra -Werror $ex/ft_print_params.c"
-gcc -Wall -Wextra -Werror $1/ft_print_params.c
+cd $1
+echo "\ngcc -Wall -Wextra -Werror $1/ft_print_params.c"
+gcc -Wall -Wextra -Werror ft_print_params.c ../../files/ft_putchar.c
+cd ..
 echo "./a.out test1 test2 test3 > user_output"
-./a.out test1 test2 test3 > user_output
+$1/a.out test1 test2 test3 > user_output
 echo >> user_output
-./a.out test4 test5 >> user_output
+echo "./a.out test4 test5"
+$1/a.out test4 test5 >> user_output
 echo >> user_output
